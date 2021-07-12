04d94c5e42460f0666e896b2961e996d442e8520
Ivan <i.klishevskiy@mail.ru>
2021-07-12 21:42:50 +0300

    Class work(1,2)

--- src/Unit1/Unit1Task7.java
+++ src/Unit1/Unit1Task7.java
@@ -2,7 +2,7 @@ package Unit1;
 
 public class Unit1Task7 {
     public static void main(String[] args) {
-        int count =504;
+        int count =100885;
         int lust = count%10;
         System.out.print(count+" программист");
         if(lust>1&&lust<5||lust==0)
---
+++ src/classwork/p2cl2.java
@@ -0,0 +1,16 @@
+package classwork;
+
+import java.util.Scanner;
+
+public class p2cl2 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        int a=scan.nextInt();
+        long f=1l;
+        for(int i=1;i<=a;i++){
+            f*=i;
+        }
+        System.out.println(f);
+    }
+}
