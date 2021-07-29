65be6c91cc7ff6dcb29f528e3f320017904fa4e3
Ivan <i.klishevskiy@mail.ru>
2021-07-29 18:23:11 +0300

    Home Work 7

---
+++ src/Unit7/robot/legs/SamsungLeg.java
@@ -0,0 +1,22 @@
+package Unit7.robot.legs;
+
+public class SamsungLeg implements ILeg{
+    private int price;
+
+    public SamsungLeg(int price) {
+        this.price = price;
+    }
+
+    public SamsungLeg() {
+    }
+
+    @Override
+    public void step() {
+        System.out.println("Шагает нога Samsung");
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+}
