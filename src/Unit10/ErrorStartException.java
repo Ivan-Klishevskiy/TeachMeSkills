50002e53e2eb8fbb2418c70b9339728bdd035989
Ivan <i.klishevskiy@mail.ru>
2021-08-18 12:44:28 +0300

    Home Work 10

---
+++ src/Unit10/ErrorStartException.java
@@ -0,0 +1,10 @@
+package Unit10;
+
+public class ErrorStartException extends Exception {
+    public ErrorStartException() {
+    }
+
+    public ErrorStartException(String message) {
+        super(message);
+    }
+}
---
+++ src/Unit10/Main.java
@@ -0,0 +1,16 @@
+package Unit10;
+
+public class Main {
+    public static void main(String[] args) {
+        Car bmw = new Car("BMW", 250, 37000);
+        Car tesla = new Car("Tesla", 400, 29950);
+
+        try {
+            bmw.start();
+            tesla.start();
+        } catch (ErrorStartException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
