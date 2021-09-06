4531f8f38a5467c1186ced05e259c3275d8d6c9f
Ivan <i.klishevskiy@mail.ru>
2021-09-06 11:33:47 +0300

    Fix

--- src/ShopUnit12/Shop.java
+++ src/ShopUnit12/Shop.java
@@ -27,31 +27,46 @@ public class Shop implements Serializable {
     }
 
     public void addProduct(Product product) {
-        if (list.stream().noneMatch(x -> x.getId() == product.getId())) {
-            product.setHistoryOfAdding(LocalTime.now());
-            list.add(product);
-            System.out.println("Добавление успешно");
-        } else {
-            System.out.println("Добавление не успешно");
-        }
+        list.stream()
+                .filter(i -> i.getId() == product.getId())
+                .findFirst()
+                .ifPresentOrElse(
+                        i
+                                -> {
+                            System.out.println("Добавление не успешно");
+                        },
+                        ()
+                                -> {
+                            product.setHistoryOfAdding(LocalTime.now());
+                            list.add(product);
+                        });
+
     }
 
     public void removeProduct(int id) {
-        list=list.stream().filter(i->i.getId()!=id).collect(Collectors.toList());
+        list.stream()
+                .filter(i -> i.getId() == id)
+                .findFirst()
+                .ifPresentOrElse(
+                        i
+                                -> {
+                            list.remove(i);
+                            System.out.println("Удаление успешно");
+                        },
+                        () -> System.out.println("Удаление не успешно"));
     }
 
     public void editProduct(Product product) {
         list.stream().
                 filter(i -> i.getId() == product.getId()).
                 findFirst().
-                ifPresentOrElse((i) -> {
+                ifPresentOrElse(
+                        (i) -> {
                             i.setName(product.getName());
                             i.setPrice(product.getPrice());
                             System.out.println("Редактирование успешно");
                         },
-                        () -> {
-                            System.out.println("Редактирование не успешно");
-                        });
+                        () -> System.out.println("Редактирование не успешно"));
     }
 
     public void setList(List<Product> list) {
@@ -69,7 +84,7 @@ public class Shop implements Serializable {
     public void printList() {
         System.out.println("Все товары:");
         System.out.println("| Название  | цена |  ID  |");
-        list.forEach(i->System.out.printf("|%10s | %5d| %5d|\n", i.getName(), i.getPrice(), i.getId()));
+        list.forEach(i -> System.out.printf("|%10s | %5d| %5d|\n", i.getName(), i.getPrice(), i.getId()));
     }
 
     @Override
